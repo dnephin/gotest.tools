289f724a0404def81f1638a807ec608c883faa5c
Daniel Nephin <dnephin@gmail.com>
2019-05-13 20:44:28 -0400

    Add golden examples

--- golden/example_test.go
+++ golden/example_test.go
@@ -1,16 +1,53 @@
 package golden_test
 
 import (
-	"testing"
-
 	"gotest.tools/assert"
 	"gotest.tools/golden"
+	"gotest.tools/internal/examplet"
 )
 
-var t = &testing.T{}
+var t = examplet.T
 
 func ExampleAssert() {
-	golden.Assert(t, "foo", "foo-content.golden")
+	actual := `
+Sometimes there is a difference.
+
+Sometimes there is not.
+`
+	golden.Assert(t, actual, "example-assert.golden")
+}
+
+func ExampleAssert_failure() {
+	actual := `
+Sometimes there is a difference.
+
+This is one of those times.
+`
+	golden.Assert(t, actual, "example-assert.golden")
+	// Output:
+	// assertion failed:
+	// --- expected
+	// +++ actual
+	// @@ -2,4 +2,4 @@
+	//  Sometimes there is a difference.
+	//
+	// -Sometimes there is not.
+	// +This is one of those times.
+}
+
+func ExampleAssert_failureInWhitespace() {
+	actual := `
+Sometimes  it  is just whitespace. 
+`
+	golden.Assert(t, actual, "example-assert-whitespace.golden")
+	// Output:
+	// assertion failed:
+	// --- expected
+	// +++ actual
+	// @@ -1,3 +1,3 @@
+	//
+	// -Sometimes·it·is·just·whitespace.
+	// +Sometimes··it··is·just·whitespace.·
 }
 
 func ExampleString() {
--- icmd/example_test.go
+++ icmd/example_test.go
@@ -7,7 +7,7 @@ import (
 	"gotest.tools/internal/examplet"
 )
 
-var t = &examplet.T
+var t = examplet.T
 
 func ExampleRunCommand() {
 	result := icmd.RunCommand("bash", "-c", "echo all good")
@@ -26,13 +26,14 @@ func ExampleRunCmd_failure() {
 	result := icmd.RunCmd(icmd.Command("cat", "/does/not/exist"))
 	result.Assert(t, icmd.Success)
 	// Output:
-	// assertion failed:
+	// assertion failed: 
 	// Command:  cat /does/not/exist
 	// ExitCode: 1
 	// Error:    exit status 1
 	// Stdout:
-	// Stderr:   cat: /does/not/exist: No such file or directory
-	//
+	// Stderr:   cat: can't open '/does/not/exist': No such file or directory
+	// 
+	// 
 	// Failures:
 	// ExitCode was 1 expected 0
 	// Expected no error
